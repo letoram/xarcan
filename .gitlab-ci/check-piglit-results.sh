ce2f24c510aa6f4856d81d796ef34ac97d78f500
Peter Hutterer <peter.hutterer@who-t.net>
2024-02-20 09:36:23 +1000

    CI: switch to the meson-build.sh helper script
    This script does the same thing but has a few more toggles than our
    custom script which means we can set everything we need in the
    gitlab-ci.yml file and have it DTRT.
    
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1317>

--- .gitlab-ci/check-piglit-results.sh
+++ .gitlab-ci/check-piglit-results.sh
@@ -4,7 +4,8 @@ set -e
 set -o xtrace
 
 if [[ -z "$MESON_BUILDDIR" ]]; then
-    MESON_BUILDDIR=build
+    echo "\$MESON_BUILDDIR not set"
+    exit 1
 fi
 
 check_piglit_results ()
@@ -23,12 +24,6 @@ check_piglit_results ()
     echo Expected $EXPECTED_RESULTS does not exist
     exit 1
 }
-
-meson -Dc_args="-fno-common" -Dprefix=/usr -Dxephyr=true -Dwerror=true $MESON_EXTRA_OPTIONS "$MESON_BUILDDIR/"
-
-export PIGLIT_DIR=/root/piglit XTEST_DIR=/root/xts LP_NUM_THREADS=0
-ninja -j${FDO_CI_CONCURRENT:-4} -C "$MESON_BUILDDIR/"
-meson test --num-processes ${FDO_CI_CONCURRENT:-4} --print-errorlogs -C "$MESON_BUILDDIR/"
 
 check_piglit_results xephyr-glamor hw/kdrive/ephyr/Xephyr.p/ephyr_glamor.c.o
 check_piglit_results xvfb hw/vfb/Xvfb
