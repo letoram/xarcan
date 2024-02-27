3e9ac852bc8ddb10459790b37c4c7a2145a6a80b
Enrico Weigelt, metux IT consult <info@metux.net>
2024-02-27 17:48:08 +0100

    os: move alloc functions to separate source file
    Reduce the massive os/utils.c a little bit by moving out the alloc
    functions to their own source file.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1336>

--- os/utils.c
+++ os/utils.c
@@ -1131,56 +1131,6 @@ set_font_authorizations(char **authorizations, int *authlen, void *client)
 #endif                          /* TCPCONN */
 }
 
-void *
-XNFalloc(unsigned long amount)
-{
-    void *ptr = malloc(amount);
-
-    if (!ptr)
-        FatalError("Out of memory");
-    return ptr;
-}
-
-/* The original XNFcalloc was used with the xnfcalloc macro which multiplied
- * the arguments at the call site without allowing calloc to check for overflow.
- * XNFcallocarray was added to fix that without breaking ABI.
- */
-void *
-XNFcalloc(unsigned long amount)
-{
-    return XNFcallocarray(1, amount);
-}
-
-void *
-XNFcallocarray(size_t nmemb, size_t size)
-{
-    void *ret = calloc(nmemb, size);
-
-    if (!ret)
-        FatalError("XNFcalloc: Out of memory");
-    return ret;
-}
-
-void *
-XNFrealloc(void *ptr, unsigned long amount)
-{
-    void *ret = realloc(ptr, amount);
-
-    if (!ret)
-        FatalError("XNFrealloc: Out of memory");
-    return ret;
-}
-
-void *
-XNFreallocarray(void *ptr, size_t nmemb, size_t size)
-{
-    void *ret = reallocarray(ptr, nmemb, size);
-
-    if (!ret)
-        FatalError("XNFreallocarray: Out of memory");
-    return ret;
-}
-
 char *
 Xstrdup(const char *s)
 {
