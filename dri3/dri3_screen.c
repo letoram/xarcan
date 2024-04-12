pelya/xserver-xsdl

commit a42992a4cca49cedd3930f5694c7a16e4f614b36

dri3: rework format/modifier caching
Cut down the unnecessary malloc/memcpy/free by utilising the explicit
copy provided by the client.

But above all: do so, after ensuring we get valid data from the
implementation.

Fixes: cef12efc15c ("glamor: Implement GetSupportedModifiers")
Cc: Louis-Francis Ratté-Boulianne <lfrb@collabora.com>
Cc: Daniel Stone <daniels@collabora.com>
Reviewed-by: Adam Jackson <ajax@redhat.com>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

dri3/dri3_screen.c
@@ -159,8 +159,10 @@ cache_formats_and_modifiers(ScreenPtr screen)
 {
     dri3_screen_priv_ptr        ds = dri3_screen_priv(screen);
     const dri3_screen_info_rec *info = ds->info;
-    CARD32                     *formats = NULL;
-    CARD64                     *modifiers = NULL;
+    CARD32                      num_formats;
+    CARD32                     *formats;
+    CARD32                      num_modifiers;
+    CARD64                     *modifiers;
     int                         i;
 
     if (ds->formats_cached)
@@ -176,34 +178,36 @@ cache_formats_and_modifiers(ScreenPtr screen)
         return Success;
     }
 
-    (*info->get_formats) (screen, &ds->num_formats, &formats);
-    ds->formats = calloc(ds->num_formats, sizeof(dri3_dmabuf_format_rec));
+    if (!info->get_formats(screen, &num_formats, &formats))
+        return BadAlloc;
+
+    if (!num_formats) {
+        ds->num_formats = 0;
+        ds->formats_cached = TRUE;
+        return Success;
+    }
+
+    ds->formats = calloc(num_formats, sizeof(dri3_dmabuf_format_rec));
     if (!ds->formats)
         return BadAlloc;
 
-    for (i = 0; i < ds->num_formats; i++) {
+    for (i = 0; i < num_formats; i++) {
         dri3_dmabuf_format_ptr iter = &ds->formats[i];
 
+        if (!info->get_modifiers(screen, formats[i],
+                                 &num_modifiers,
+                                 &modifiers))
+            continue;
+
+        if (!num_modifiers)
+            continue;
+
         iter->format = formats[i];
-        (*info->get_modifiers) (screen, formats[i],
-                                &iter->num_modifiers,
-                                &modifiers);
-
-        iter->modifiers = malloc(iter->num_modifiers * sizeof(CARD64));
-        if (iter->modifiers == NULL)
-            goto error;
-
-        memcpy(iter->modifiers, modifiers,
-               iter->num_modifiers * sizeof(CARD64));
-        goto done;
-
-error:
-        iter->num_modifiers = 0;
-        free(iter->modifiers);
-done:
-        free(modifiers);
+        iter->num_modifiers = num_modifiers;
+        iter->modifiers = modifiers;
     }
-    free(formats);
+
+    ds->num_formats = i;
     ds->formats_cached = TRUE;
 
     return Success;

letoram/xarcan

commit 3b6b88c184ddbe41ddab91a543e08fddcef14481

dri3: Free formats in cache_formats_and_modifiers
Fixes leak:

==15672== 60 bytes in 1 blocks are definitely lost in loss record 3,803 of 8,127
==15672==    at 0x4840718: malloc (vg_replace_malloc.c:392)
==15672==    by 0x2F2698: XNFreallocarray (alloc.c:55)
==15672==    by 0x1ADAA9: xwl_dmabuf_get_formats_for_device (xwayland-dmabuf.c:207)
==15672==    by 0x1ADAA9: xwl_glamor_get_formats (xwayland-dmabuf.c:248)
==15672==    by 0x303D86: cache_formats_and_modifiers (dri3_screen.c:176)
==15672==    by 0x303D86: dri3_get_supported_modifiers (dri3_screen.c:229)
==15672==    by 0x30331A: proc_dri3_get_supported_modifiers (dri3_request.c:389)
==15672==    by 0x217B6B: Dispatch (dispatch.c:550)
==15672==    by 0x21B9A0: dix_main (main.c:276)
==15672==    by 0x51086C9: (below main) (libc_start_call_main.h:58)

Fixes: a42992a4cca4 ("dri3: rework format/modifier caching")
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1484>

dri3/dri3_screen.c
@@ -183,8 +183,10 @@ cache_formats_and_modifiers(ScreenPtr screen)
     }
 
     ds->formats = calloc(num_formats, sizeof(dri3_dmabuf_format_rec));
-    if (!ds->formats)
+    if (!ds->formats) {
+        free(formats);
         return BadAlloc;
+    }
 
     for (i = 0; i < num_formats; i++) {
         dri3_dmabuf_format_ptr iter = &ds->formats[i];
@@ -205,6 +207,7 @@ cache_formats_and_modifiers(ScreenPtr screen)
     ds->num_formats = i;
     ds->formats_cached = TRUE;
 
+    free(formats);
     return Success;
 }
 