pelya/xserver-xsdl

commit 30ac7567980a1eb79d084a63e0e74e1d9a3af673

Create a threaded mechanism for input [v7]
The current SIGIO signal handler method, used at generation of input events,
has a bunch of oddities. This patch introduces an alternative way using a
thread, which is used to select() all input device file descriptors.

A mutex was used to control the access to input structures by the main and input
threads. Two pipes to emit alert events (such hotplug ones) and guarantee the
proper communication between them was also used.

Co-authored-by: Fernando Carrijo <fcarrijo@freedesktop.org>
Signed-off-by: Tiago Vignatti <tiago.vignatti@nokia.com>

v2: Fix non-Xorg link. Enable where supported by default.

    This also splits out the actual enabling of input threads to
    DDX-specific patches which follow

v3: Make the input lock recursive

v4: Use regular RECURSIVE_MUTEXes instead of rolling our own
    Respect the --disable-input-thread configuration option by
    providing stubs that expose the same API/ABI.

    Respond to style comments from Peter Hutterer.

v5: use __func__ in inputthread debug and error mesages.

    Respond to style comments from Peter Hutterer.

v6: use AX_PTHREAD instead of inlining pthread tests.

    Suggested by Emil Velikov <emil.l.velikov@gmail.com>

v7: Use pthread_sigmask instead of sigprocmask when using threads

    Suggested by Adam Jackson <ajax@redhat.com>

Signed-off-by: Adam Jackson <ajax@redhat.com>
Signed-off-by: Keith Packard <keithp@keithp.com>
Reviewed-by: Adam Jackson <ajax@redhat.com>

hw/xfree86/os-support/shared/sigio.c
@@ -137,7 +137,7 @@ xf86BlockSIGIO(void)
 
     sigemptyset(&set);
     sigaddset(&set, SIGIO);
-    sigprocmask(SIG_BLOCK, &set, NULL);
+    xthread_sigmask(SIG_BLOCK, &set, NULL);
 }
 
 static void
@@ -147,7 +147,7 @@ xf86ReleaseSIGIO(void)
 
     sigemptyset(&set);
     sigaddset(&set, SIGIO);
-    sigprocmask(SIG_UNBLOCK, &set, NULL);
+    xthread_sigmask(SIG_UNBLOCK, &set, NULL);
 }
 
 int

include/os.h
@@ -706,4 +706,9 @@ xorg_backtrace(void);
 extern _X_EXPORT int
 os_move_fd(int fd);
 
+#include <signal.h>
+
+extern _X_EXPORT int
+xthread_sigmask(int how, const sigset_t *set, sigset_t *oldest);
+
 #endif                          /* OS_H */

configure.ac
@@ -832,6 +832,25 @@ SDK_REQUIRED_MODULES="$XPROTO $RANDRPROTO $RENDERPROTO $XEXTPROTO $INPUTPROTO $K
 # Make SDK_REQUIRED_MODULES available for inclusion in xorg-server.pc
 AC_SUBST(SDK_REQUIRED_MODULES)
 
+AC_CHECK_DECL([PTHREAD_MUTEX_RECURSIVE], [HAVE_RECURSIVE_MUTEX=yes], [HAVE_RECURSIVE_MUTEX=no], [[#include <pthread.h>]])
+
+THREAD_DEFAULT=no
+
+if test "x$HAVE_RECURSIVE_MUTEX" = "xyes" ; then
+	THREAD_DEFAULT=yes
+fi
+
+AC_ARG_ENABLE(input-thread, AS_HELP_STRING([--enable-input-thread],
+	     [Enable input threads]),
+	     [INPUTTHREAD=$enableval], [INPUTTHREAD=$THREAD_DEFAULT])
+
+if test "x$INPUTTHREAD" = "xyes" ; then
+    AX_PTHREAD(,AC_MSG_ERROR([threaded input requested but no pthread support has been found]))
+    SYS_LIBS="$SYS_LIBS $PTHREAD_LIBS"
+    CFLAGS="$CFLAGS $PTHREAD_CFLAGS"
+    AC_DEFINE(INPUTTHREAD, 1, [Use a separate input thread])
+fi
+
 REQUIRED_MODULES="$FIXESPROTO $DAMAGEPROTO $XCMISCPROTO $XTRANS $BIGREQSPROTO $SDK_REQUIRED_MODULES"
 
 dnl systemd socket activation

dix/globals.c
@@ -132,3 +132,12 @@ Bool explicit_display = FALSE;
 char *ConnectionInfo;
 
 CARD32 TimeOutValue = DEFAULT_TIMEOUT * MILLI_PER_SECOND;
+
+#if DEBUG_INPUT_MUTEX
+#define INPUT_MUTEX_INITIALIZER PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP
+#else
+#define INPUT_MUTEX_INITIALIZER PTHREAD_MUTEX_INITIALIZER
+#endif
+
+pthread_mutex_t input_mutex = INPUT_MUTEX_INITIALIZER;
+__thread int input_mutex_count;

dix/main.c
@@ -121,12 +121,9 @@ Equipment Corporation.
 extern void Dispatch(void);
 
 #ifdef XQUARTZ
-#include <pthread.h>
-
-BOOL serverRunning = FALSE;
+BOOL serverRunning;
 pthread_mutex_t serverRunningMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t serverRunningCond = PTHREAD_COND_INITIALIZER;
-
 #endif
 
 CallbackListPtr RootWindowFinalizeCallback = NULL;
@@ -299,6 +296,8 @@ dix_main(int argc, char *argv[], char *envp[])
 
         NotifyParentProcess();
 
+        InputThreadInit();
+
         Dispatch();
 
 #ifdef XQUARTZ
@@ -330,6 +329,8 @@ dix_main(int argc, char *argv[], char *envp[])
 #endif
 
         CloseInput();
+
+        InputThreadFini();
 
         for (i = 0; i < screenInfo.numScreens; i++)
             screenInfo.screens[i]->root = NullWindow;

hw/dmx/input/dmxsigio.c
@@ -99,7 +99,7 @@ dmxSigioHook(void)
     sigaction(SIGIO, &a, 0);
 
     sigemptyset(&s);
-    sigprocmask(SIG_SETMASK, &s, 0);
+    xthread_sigmask(SIG_SETMASK, &s, 0);
 }
 
 static void

include/dix-config.h.in
@@ -524,4 +524,7 @@
 /* Have posix_fallocate() */
 #undef HAVE_POSIX_FALLOCATE
 
+/* Use input thread */
+#undef INPUTTHREAD
+
 #endif /* _DIX_CONFIG_H_ */

include/input.h
@@ -56,6 +56,7 @@ SOFTWARE.
 #include "xkbrules.h"
 #include "events.h"
 #include "list.h"
+#include "os.h"
 #include <X11/extensions/XI2.h>
 
 #define DEVICE_INIT	0
@@ -714,13 +715,20 @@ extern _X_HIDDEN void input_constrain_cursor(DeviceIntPtr pDev, ScreenPtr screen
                                              int *out_x, int *out_y,
                                              int *nevents, InternalEvent* events);
 
-static inline void input_lock(void) {
-}
+extern _X_EXPORT void input_lock(void);
+extern _X_EXPORT void input_unlock(void);
+extern _X_EXPORT void input_force_unlock(void);
 
-static inline void input_unlock(void) {
-}
+extern void InputThreadPreInit(void);
+extern void InputThreadInit(void);
+extern void InputThreadFini(void);
 
-static inline void input_force_unlock(void) {
-}
+extern int InputThreadRegisterDev(int fd,
+                                  NotifyFdProcPtr readInputProc,
+                                  void *readInputArgs);
+
+extern int InputThreadUnregisterDev(int fd);
+
+extern _X_EXPORT Bool InputThreadEnable;
 
 #endif                          /* INPUT_H */

letoram/xarcan

commit 3b7a63e6ae5c59127bab1ff10acb51003ced0b53

os: unexport xthread_sigmask
This function isn't used by drivers and there's currently no need to do so,
thus keep it out of the public module API.

Fixes: 30ac7567980a1eb79d084a63e0e74e1d9a3af673
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1368>

hw/xfree86/os-support/shared/sigio.c
@@ -59,6 +59,8 @@
 #include <errno.h>
 #include <X11/X.h>
 
+#include "os/osdep.h"
+
 #include <xserver_poll.h>
 #include "xf86.h"
 #include "xf86Priv.h"

include/os.h
@@ -510,7 +510,4 @@ xorg_backtrace(void);
 typedef _sigset_t sigset_t;
 #endif
 
-extern _X_EXPORT int
-xthread_sigmask(int how, const sigset_t *set, sigset_t *oldest);
-
 #endif                          /* OS_H */

os/osdep.h
@@ -166,4 +166,8 @@ void AutoResetServer(int sig);
 /* clone fd so it gets out of our select mask */
 int os_move_fd(int fd);
 
+/* set signal mask - either on current thread or whole process,
+   depending on whether multithreading is used */
+int xthread_sigmask(int how, const sigset_t *set, sigset_t *oldest);
+
 #endif                          /* _OSDEP_H_ */