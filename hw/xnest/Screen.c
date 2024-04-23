letoram/xarcan

commit aa3f5023e3fae0df74039702b6c8218bc14dc679

xnest/mi: remove redundant call of miScreenDevPrivateInit()
miScreenDevPrivateInit() is also made static in this commit.

Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/854>

hw/xnest/Screen.c
@@ -242,8 +242,9 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
 
     /* myNum */
     /* id */
-    miScreenInit(pScreen, NULL, xnestWidth, xnestHeight, 1, 1, xnestWidth, rootDepth, numDepths, depths, defaultVisual, /* root visual */
-                 numVisuals, visuals);
+    if (!miScreenInit(pScreen, NULL, xnestWidth, xnestHeight, 1, 1, xnestWidth, rootDepth, numDepths, depths, defaultVisual, /* root visual */
+                      numVisuals, visuals))
+        return FALSE;
 
     pScreen->defColormap = (Colormap) FakeClientID(0);
     pScreen->minInstalledCmaps = MINCMAPS;
@@ -331,9 +332,6 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
 
     /* overwrite miCloseScreen with our own */
     pScreen->CloseScreen = xnestCloseScreen;
-
-    if (!miScreenDevPrivateInit(pScreen, xnestWidth, NULL))
-        return FALSE;
 
     /* overwrite miSetShape with our own */
     pScreen->SetShape = xnestSetShape;

mi/miscrinit.c
@@ -184,7 +184,7 @@ miCreateScreenResources(ScreenPtr pScreen)
     return TRUE;
 }
 
-Bool
+static Bool
 miScreenDevPrivateInit(ScreenPtr pScreen, int width, void *pbits)
 {
     miScreenInitParmsPtr pScrInitParms;

mi/mi.h
@@ -382,11 +382,6 @@ extern _X_EXPORT Bool miModifyPixmapHeader(PixmapPtr pPixmap,
 extern _X_EXPORT Bool miCreateScreenResources(ScreenPtr /*pScreen */
     );
 
-extern _X_EXPORT Bool miScreenDevPrivateInit(ScreenPtr pScreen,
-                                             int width,
-                                             void *pbits
-    );
-
 extern _X_EXPORT Bool miScreenInit(ScreenPtr pScreen,
                                    void *pbits,
                                    int xsize,

commit 94451181c2d6f65d761cb79020a4de2909ece353

xnest: fix segfault in miCreateScreenResources()
With aa3f5023e3fae0df74039702b6c8218bc14dc679, pScreen->devPrivate now is
initialized only once, which uncovered a silent bug in xnestOpenScreen:
It's NULL'ing the pScreen->devPrivate pointer which already had been
initialized by previous miScreenDevPrivateInit() call.

Fixes: aa3f5023e3fae0df74039702b6c8218bc14dc679
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1495>

hw/xnest/Screen.c
@@ -20,6 +20,8 @@ is" without express or implied warranty.
 #include <X11/Xdefs.h>
 #include <X11/Xproto.h>
 
+#include "mi/mi_priv.h"
+
 #include "scrnintstr.h"
 #include "dix.h"
 #include "mi.h"
@@ -257,7 +259,6 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
     pScreen->blackPixel = xnestBlackPixel;
     /* GCperDepth */
     /* defaultStipple */
-    pScreen->devPrivate = NULL;
     /* WindowPrivateLen */
     /* WindowPrivateSizes */
     /* totalWindowSize */
@@ -419,7 +420,7 @@ xnestCloseScreen(ScreenPtr pScreen)
         free(pScreen->allowedDepths[i].vids);
     free(pScreen->allowedDepths);
     free(pScreen->visuals);
-    free(pScreen->devPrivate);
+    miScreenClose(pScreen);
 
     /*
        If xnestDoFullGeneration all x resources will be destroyed upon closing

mi/mi_priv.h
@@ -1,0 +1,12 @@
+/* SPDX-License-Identifier: MIT OR X11
+ *
+ * Copyright © 2024 Enrico Weigelt, metux IT consult <info@metux.net>
+ */
+#ifndef _XSERVER_MI_PRIV_H
+#define _XSERVER_MI_PRIV_H
+
+#include "screenint.h"
+
+void miScreenClose(ScreenPtr pScreen);
+
+#endif /* _XSERVER_MI_PRIV_H */

mi/miscrinit.c
@@ -31,6 +31,9 @@ from The Open Group.
 #endif
 
 #include <X11/X.h>
+
+#include "mi/mi_priv.h"
+
 #include "servermd.h"
 #include "misc.h"
 #include "mi.h"
@@ -308,4 +311,12 @@ miSetZeroLineBias(ScreenPtr pScreen, unsigned int bias)
 
     dixSetPrivate(&pScreen->devPrivates, miZeroLineScreenKey,
                   (unsigned long *) (unsigned long) bias);
+}
+
+void miScreenClose(ScreenPtr pScreen)
+{
+    if (pScreen->devPrivate) {
+        free(pScreen->devPrivate);
+        pScreen->devPrivate = NULL;
+    }
 }