letoram/xarcan

commit be4c8444eb0cc9c96682e2650d45f028b9a13b6c

os: unexport Os*() functions
These aren't called (and suited for being called) by drivers,
thus drop them from the public module API.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1381>

os/osinit.c
@@ -48,14 +48,11 @@ SOFTWARE.
 #include <dix-config.h>
 #endif
 
-#include <stdio.h>
-#include <X11/X.h>
-#include "os.h"
-#include "osdep.h"
-#include "opaque.h"
-#include <X11/Xos.h>
-#include <signal.h>
 #include <errno.h>
+#include <stdio.h>
+#include <signal.h>
+#include <X11/X.h>
+#include <X11/Xos.h>
 #ifdef HAVE_DLFCN_H
 #include <dlfcn.h>
 #endif
@@ -64,9 +61,13 @@ SOFTWARE.
 #endif
 
 #include "os/busfault.h"
+#include "os/osdep.h"
 
 #include "misc.h"
 
+#include "os.h"
+#include "opaque.h"
+#include "misc.h"
 #include "dixstruct.h"
 
 #if !defined(SYSV) && !defined(WIN32)

Xext/shm.c
@@ -46,6 +46,7 @@ in this Software without prior written authorization from The Open Group.
 
 #include "os/auth.h"
 #include "os/busfault.h"
+#include "os/osdep.h"
 
 #include "misc.h"
 #include "os.h"

composite/compwindow.c
@@ -46,6 +46,7 @@
 #endif
 
 #include "dix/dix_priv.h"
+#include "os/osdep.h"
 
 #include "compint.h"
 #include "compositeext_priv.h"

dix/main.c
@@ -90,6 +90,7 @@ Equipment Corporation.
 #include "os/audit.h"
 #include "os/auth.h"
 #include "os/cmdline.h"
+#include "os/osdep.h"
 
 #include "scrnintstr.h"
 #include "misc.h"

hw/kdrive/ephyr/ephyr.c
@@ -30,8 +30,9 @@
 #include <xcb/xcb_keysyms.h>
 #include <X11/keysym.h>
 
-#include "ephyr.h"
+#include "os/osdep.h"
 
+#include "ephyr.h"
 #include "inputstr.h"
 #include "scrnintstr.h"
 #include "ephyrlog.h"

hw/kdrive/ephyr/ephyrinit.c
@@ -28,6 +28,7 @@
 #endif
 
 #include "os/cmdline.h"
+#include "os/osdep.h"
 
 #include "ephyr.h"
 #include "ephyrlog.h"

hw/kdrive/src/kdrive.c
@@ -27,6 +27,8 @@
 #include "dix/screenint_priv.h"
 #include "os/cmdline.h"
 
+#include "os/osdep.h"
+
 #include "kdrive.h"
 #include <mivalidate.h>
 #include <dixstruct.h>

hw/vfb/InitOutput.c
@@ -40,6 +40,7 @@ from The Open Group.
 
 #include "dix/screenint_priv.h"
 #include "os/cmdline.h"
+#include "os/osdep.h"
 
 #include "scrnintstr.h"
 #include "servermd.h"

commit 61d0c8e21c82198f5f3f1bd373e18166959677fb

os: drop duplicate include of misc.h
Need to include it only once.

Fixes: be4c8444eb0cc9c96682e2650d45f028b9a13b6c
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1542>

os/osinit.c
@@ -66,7 +66,6 @@ SOFTWARE.
 #include "misc.h"
 #include "os.h"
 #include "opaque.h"
-#include "misc.h"
 #include "dixstruct.h"
 #include "dixstruct_priv.h"
 