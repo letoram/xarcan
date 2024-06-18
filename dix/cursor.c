pelya/xserver-xsdl

commit 0afeb0241a83796575da827bd81375c99ff10af5

DIX: Clean up null root cursor handling
Move the null root cursor handling out of main() and into CreateRootCursor.

dix/cursor.c
@@ -430,25 +430,41 @@ AllocGlyphCursor(Font source, unsigned sourceChar, Font mask, unsigned maskChar,
  *************************************************************/
 
 CursorPtr 
-CreateRootCursor(char *pfilename, unsigned glyph)
+CreateRootCursor(char *unused1, unsigned int unused2)
 {
     CursorPtr 	curs;
+#ifdef NULL_ROOT_CURSOR
+    CursorMetricRec cm;
+#else
     FontPtr 	cursorfont;
     int	err;
     XID		fontID;
+#endif
 
+#ifdef NULL_ROOT_CURSOR
+    cm.width = 0;
+    cm.height = 0;
+    cm.xhot = 0;
+    cm.yhot = 0;
+
+    curs = AllocCursor(NULL, NULL, &cm, 0, 0, 0, 0, 0, 0);
+
+    if (curs == NullCursor)
+        return NullCursor;
+#else
     fontID = FakeClientID(0);
     err = OpenFont(serverClient, fontID, FontLoadAll | FontOpenSync,
-	(unsigned)strlen( pfilename), pfilename);
+	(unsigned)strlen(defaultCursorFont), defaultCursorFont);
     if (err != Success)
 	return NullCursor;
 
     cursorfont = (FontPtr)LookupIDByType(fontID, RT_FONT);
     if (!cursorfont)
 	return NullCursor;
-    if (AllocGlyphCursor(fontID, glyph, fontID, glyph + 1,
+    if (AllocGlyphCursor(fontID, 0, fontID, 1,
 			 0, 0, 0, ~0, ~0, ~0, &curs, serverClient) != Success)
 	return NullCursor;
+#endif
 
     if (!AddResource(FakeClientID(0), RT_CURSOR, (pointer)curs))
 	return NullCursor;

dix/main.c
@@ -411,22 +411,10 @@ main(int argc, char *argv[], char *envp[])
 	    FatalError("could not open default font '%s'", defaultTextFont);
 	}
 
-#ifdef NULL_ROOT_CURSOR
-        cm.width = 0;
-        cm.height = 0;
-        cm.xhot = 0;
-        cm.yhot = 0;
-
-        if (!(rootCursor = AllocCursor(NULL, NULL, &cm, 0, 0, 0, 0, 0, 0))) {
-            FatalError("could not create empty root cursor");
-	}
-        AddResource(FakeClientID(0), RT_CURSOR, (pointer)rootCursor);
-#else
-	if (!(rootCursor = CreateRootCursor(defaultCursorFont, 0))) {
+	if (!(rootCursor = CreateRootCursor(NULL, 0))) {
 	    FatalError("could not open default cursor font '%s'",
 		       defaultCursorFont);
 	}
-#endif
 
 #ifdef DPMSExtension
  	/* check all screens, looking for DPMS Capabilities */

letoram/xarcan

commit 4a27ff7a1a3f7acfa7d4b97e495a3e331e8f6ef4

dix: drop unused args from CreateRootCursor()
The args have become dummies almost two decades ago, so no need
to keep them any longer.

Fixes: 0afeb0241a83796575da827bd81375c99ff10af5
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1522>

dix/cursor.c
@@ -489,7 +489,7 @@ AllocGlyphCursor(Font source, unsigned sourceChar, Font mask, unsigned maskChar,
  *************************************************************/
 
 CursorPtr
-CreateRootCursor(char *unused1, unsigned int unused2)
+CreateRootCursor(void)
 {
     CursorPtr curs;
     FontPtr cursorfont;

dix/main.c
@@ -237,7 +237,7 @@ dix_main(int argc, char *argv[], char *envp[])
             FatalError("could not open default font");
         }
 
-        if (!(rootCursor = CreateRootCursor(NULL, 0))) {
+        if (!(rootCursor = CreateRootCursor())) {
             FatalError("could not open default cursor font");
         }
 

include/cursor.h
@@ -99,8 +99,7 @@ extern _X_EXPORT int AllocGlyphCursor(Font /*source */ ,
                                       ClientPtr /*client */ ,
                                       XID /*cid */ );
 
-extern _X_EXPORT CursorPtr CreateRootCursor(char * /*pfilename */ ,
-                                            unsigned int /*glyph */ );
+extern _X_EXPORT CursorPtr CreateRootCursor(void);
 
 extern _X_EXPORT int ServerBitsFromGlyph(FontPtr /*pfont */ ,
                                          unsigned int /*ch */ ,