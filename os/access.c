a6a993f9508ff0bd6f19695b2dfaef170c652525
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2024-06-23 21:23:47 +0100

    os: Fix siHostnameAddrMatch in the case where h_addr isn't defined
    When IPv6 support isn't enabled, and h_addr isn't defined,
    there is no for loop, so the break statement is invalid.
    
    Signed-off-by: Joaquim Monteiro <joaquim.monteiro@protonmail.com>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1572>

--- os/access.c
+++ os/access.c
@@ -1867,7 +1867,9 @@ siHostnameAddrMatch(int family, void *addr, int len,
                 if ((f == family) && (len == hostaddrlen) &&
                     (memcmp(addr, hostaddr, len) == 0)) {
                     res = TRUE;
+#ifdef h_addr
                     break;
+#endif
                 }
             }
         }
