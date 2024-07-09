letoram/xarcan

commit 792758faa5c089a484ed733d76eee00ddc278177

xwayland: Update lost focus on deactivation
Use the "activated" state from xdg-shell to call the pointer and
keyboard leave events when running rootful.

The regular pointer and keyboard leave notifications are now ignored
when running rootful.

Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
Closes: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1604
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1213>

hw/xwayland/xwayland-input.c
@@ -617,7 +617,8 @@ pointer_handle_leave(void *data, struct wl_pointer *pointer,
         focus_lost = TRUE;
     }
 
-    xwl_seat_leave_ptr(xwl_seat, focus_lost);
+    if (xwl_screen->rootless)
+        xwl_seat_leave_ptr(xwl_seat, focus_lost);
 }
 
 static void
@@ -1211,7 +1212,8 @@ keyboard_handle_leave(void *data, struct wl_keyboard *keyboard,
 
     xwl_screen->serial = serial;
 
-    xwl_seat_leave_kbd(xwl_seat);
+    if (xwl_screen->rootless)
+        xwl_seat_leave_kbd(xwl_seat);
 }
 
 static void

hw/xwayland/xwayland-screen.h
@@ -54,6 +54,7 @@ struct xwl_screen {
     int expecting_event;
     enum RootClipMode root_clip_mode;
 
+    Bool active;
     int rootless;
     xwl_glamor_mode_flags glamor;
     int present;

hw/xwayland/xwayland-window.c
@@ -795,6 +795,8 @@ xdg_toplevel_handle_configure(void *data,
 {
     struct xwl_window *xwl_window = data;
     struct xwl_screen *xwl_screen = xwl_window->xwl_screen;
+    uint32_t *p;
+    Bool old_active = xwl_screen->active;
 
     /* Maintain our current size if no dimensions are requested */
     if (width == 0 && height == 0)
@@ -803,6 +805,20 @@ xdg_toplevel_handle_configure(void *data,
     if (!xwl_screen->fullscreen) {
         /* This will be committed by the xdg_surface.configure handler */
         xwl_window_maybe_resize(xwl_window, width, height);
+    }
+
+    xwl_screen->active = FALSE;
+    wl_array_for_each (p, states) {
+        uint32_t state = *p;
+        if (state == XDG_TOPLEVEL_STATE_ACTIVATED) {
+            xwl_screen->active = TRUE;
+            break;
+        }
+    }
+
+    if (old_active != xwl_screen->active) {
+        if (!xwl_screen->active)
+            xwl_screen_lost_focus(xwl_screen);
     }
 }
 

commit 72036261738c10d718cb42696289ae85b7f6f703

xwayland: Check for pointer in xwl_seat_leave_ptr()
Since commit 792758fa ("xwayland: Update lost focus on deactivation"),
in rootful mode, if we receive an "activated" state from xdg-shell
indicating that the surface is no longer active, we shall end up calling
xwl_seat_leave_ptr().

But xwl_seat_leave_ptr() does not actually check whether the seat has
pointer capabilities, and if not, get_pointer_device() will return NULL.

As a result, we would crash using a NULL pointer in GetMaster(). This
typically can happen when using Xwayland rootful on headless compositors
such as "cage" which do not advertise any capabilities for the seat.

To avoid the issue, simply check whether get_pointer_device() returns a
valid non-null device and bail out early otherwise.

Fixes: 792758fa - xwayland: Update lost focus on deactivation
Closes: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1700
Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1590>

hw/xwayland/xwayland-input.c
@@ -597,6 +597,9 @@ xwl_seat_leave_ptr(struct xwl_seat *xwl_seat, Bool focus_lost)
 {
     DeviceIntPtr dev = get_pointer_device(xwl_seat);
 
+    if (!dev)
+        return;
+
     if (focus_lost)
         CheckMotion(NULL, GetMaster(dev, POINTER_OR_FLOAT));
 